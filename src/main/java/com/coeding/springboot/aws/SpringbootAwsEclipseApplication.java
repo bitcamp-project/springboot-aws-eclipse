e3bc771da81b6fab2f580000f0d0b0b3659da17c
william jang <coedplay@gmail.com>
2024-05-22 15:36:00 +0900

    init commit

---
+++ src/main/java/com/coeding/springboot/aws/SpringbootAwsEclipseApplication.java
@@ -0,0 +1,13 @@
+package com.coeding.springboot.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsEclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsEclipseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coeding/springboot/aws/SpringbootAwsEclipseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coeding.springboot.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsEclipseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
